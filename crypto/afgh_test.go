d13faf83be1e9671ed7558555a0320df5e9c3843
Paul O'Leary <poleary@kissmetrics.com>
2018-01-02 20:48:05 -0800

    Huh - didn't add file ...?

---
+++ crypto/afgh_test.go
@@ -0,0 +1,31 @@
+package crypto
+
+import (
+	"testing"
+	"gopbc/pairing"
+	"gopbc/field"
+)
+
+func TestProxyReEncryptionBasics(t *testing.T) {
+
+	pairingParms := pairing.GetCompatParams()
+	typeAPairing := pairing.MakeTypeAPairing(pairingParms)
+
+	afgh := new(ProxyReEncryption)
+	afgh.BasePairing = typeAPairing.BasePairing
+
+	secretKey1 := afgh.GenerateSecretKey()
+	secretKey2 := afgh.GenerateSecretKey()
+	field.Trace(secretKey1, secretKey2)
+
+	publicKey1 := afgh.GeneratePublicKey(secretKey1)
+	publicKey2 := afgh.GeneratePublicKey(secretKey2)
+	field.Trace(publicKey1, publicKey2)
+
+	reEncryptKey1To2 := afgh.GenerateReEncryptionKey(secretKey1, publicKey2)
+	field.Trace(reEncryptKey1To2)
+
+	testData := []byte("Mr. Watson. Come Here. I need you.")
+	testElement := afgh.makeDataElement(testData)
+	field.Trace(testElement)
+}
